685c1a9affef8fb88f4411192789998d11b5d95c
Hoàng Viết Khánh <87082274+Kevinking16@users.noreply.github.com>
2023-04-26 15:50:58 +0700

    Add files via upload

--- src/main/java/org/example/MarioStatemachine.java
+++ src/main/java/org/example/MarioStatemachine.java
@@ -8,3 +8,20 @@ public class MarioStatemachine {
         System.out.println(marioStateMachine(wasmPath, eatThis));
     }
 }
+
+//    @Given("I have a Java program")
+//    public void i_have_a_java_program(){
+//        // Write code here that turns the phrase above into concrete actions
+////        marioStatemachine = new MarioStatemachine();\
+//        System.out.println("this is my program");
+//    }
+//    @When("I enter the string {string} as input")
+//    public void i_enter_the_string_as_input(String string) throws Throwable{
+//        input = string;
+//        output = marioStateMachine("src/main/resources/rust_mario.wasm", convert(input));
+//    }
+//    @Then("I should get {string} as output")
+//    public void i_should_get_as_output(String string) throws Throwable{
+//        // Write code here that turns the phrase above into concrete actions
+//        assertEquals(output, string);
+//    }
